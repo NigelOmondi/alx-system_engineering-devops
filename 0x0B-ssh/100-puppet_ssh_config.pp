5348cce46b6d04f3a38df8463486b4d74ce21612
NigelOmondi <nigelomondi62@gmail.com>
2023-08-27 21:15:05 +0300

    Create a client configuration file with Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,13 @@
-# Define the SSH configuration path
-
-file { '/home/nigel/.ssh/config':
-  ensure => 'file',
-  mode   => '0600',
-}
 # Add SSH client configuration lines
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/home/nigel/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare the identity file':
   ensure => present,
-  path   => '/home/nigel/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
 
