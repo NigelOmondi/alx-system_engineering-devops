409d16f23a5ae0343a20256160e76cd34e63933c
NigelOmondi <nigelomondi62@gmail.com>
2023-08-28 18:39:32 +0300

    Updated my ssh_config using puppet to factor in multiple-servers situation

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,19 @@
 # Add SSH client configuration lines
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
+if $facts['networking']['hostname'] == 'quantum-server' {
+  file_line { 'Turn off passwd auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+  }
 
-file_line { 'Declare the identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-}
+  file_line { 'Declare the identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school',
+  }
 
 # Notify user about the changes
-notify { 'SSH configuration updated':
-  message => 'Your SSH client configuration has been updated.',
+  notify { 'SSH configuration updated':
+    message => 'Your SSH client configuration has been updated.',
+  }
 }
