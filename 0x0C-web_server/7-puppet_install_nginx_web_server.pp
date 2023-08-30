24bc6a7643e8016761e909eafbcfb0cb6df6be20
NigelOmondi <nigelomondi62@gmail.com>
2023-08-30 04:01:08 +0300

    Install Nginx web server (w/ Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,75 @@
+# This Puppet Manifest installs and configures Nginx server
+exec { 'fetch nginx from official repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => 'usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Update the packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Install nginx web server
+package { 'nginx':
+  ensure => installed,
+}
+
+# allow HTTP protocol connetions
+exec { 'allow http':
+  command => 'ufw allow "Nginx HTTP"',
+  path    => 'usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Change folder permissions
+exec { 'the www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Create index file 
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# create another index
+file { 'var/www/html/404.html':
+  content => 'Ceci n'est pas une page\n',
+}
+
+# Add a redirection and error page
+file {'Nginx default config':
+  ensure   => file,
+  path     => '/etc/nginx/sites-enabled/default',
+  content  => 
+   "server {
+        listen 80 default server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
+        error_page /404 /404.html;
+        location /404.html {
+            internal;
+        }
+
+        if (\$request_filaname ~ redirect_me) {
+            rewrite ^ https:www.youtube.com/watch/v=QHZ-TGUwu4 permanent)
+        }
+
+    }",
+}
+
+# restart nginx
+exec { 'restart service':
+  command => 'sudo service nginx restart"
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# start service engine website
+service { "engine x":
+  ensure  => running,
+  require => Package['nginx'],
+}
