790e9baf5ec15de33d91dea773db11b533d89c12
NigelOmondi <nigelomondi62@gmail.com>
2023-09-09 15:37:54 +0300

    Configure server using puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,76 +1,50 @@
 # This Puppet Manifest installs and configures Nginx server
-exec { 'add nginx official repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => 'usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Update the packages list
-exec { 'update packages':
-  command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Install nginx web server
 package { 'nginx':
   ensure => 'installed',
 }
 
-# allow HTTP protocol connetions
-exec { 'allow HTTP':
-  command => 'ufw allow "Nginx HTTP"',
-  path    => 'usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+# Configure the Nginx server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    
+    root /var/www/html;
+    index index.html;
+    
+    location / {
+        try_files \$uri \$uri/ =404;
+        add_header Content-Type text/html;
+        echo 'Hello World!';
+    }
+    
+    location /redirect_me {
+        return 301 http://example.com;
+    }
 }
-
-# Change folder permissions
-exec { 'the www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Create index file 
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-}
-
-# create another index
-file { 'var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-# Add a redirection and error page
-file {'Nginx default config':
-  ensure  => 'file',
-  path    => '/etc/nginx/sites-enabled/default',
-  content => "server {
-        listen 80 default server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        location / {
-            try_files \$uri \$uri/ =404;
-        }
-        error_page 404 /404.html;
-        location /404.html {
-            internal;
-        }
-
-        if (\$request_filaname ~ redirect_me) {
-            rewrite ^ https:www.youtube.com/watch/v=QHZ-TGUwu4 permanent;
-        }
-
-    }",
-}
-
-# restart nginx
-exec { 'restart service':
-  command => 'sudo service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-# start service engine website
-service { 'engine x':
-  ensure  => running,
+  ",
   require => Package['nginx'],
+}
+
+# Create the Hello World HTML file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Create a symbolic link to enable the site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+# Ensure Nginx is running and enabled at boot
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => [File['/etc/nginx/sites-enabled/default'], File['/var/www/html/index.html']],
 }
