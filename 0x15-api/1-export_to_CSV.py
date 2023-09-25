7a646d71c2ab338c825a6fe78d0e7cbb22f091ce
NigelOmondi <nigelomondi62@gmail.com>
2023-09-25 16:13:19 +0300

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Exports to CSV"""
+"""Exports fetched data intoa  CSV format"""
 
-from requests import get
-from sys import argv
+import requests
+import sys
 
 
 if __name__ == '__main__':
