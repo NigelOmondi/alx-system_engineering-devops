b48ae8dbc675342139882deebdcdd83b4e352703
NigelOmondi <nigelomondi62@gmail.com>
2023-10-10 14:55:46 +0300

    Debug wordpress site

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix bad `phpp` extension to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-extension':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
